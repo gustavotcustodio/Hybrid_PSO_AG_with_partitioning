32a63b922300607a019a0a5510f86a2eb8924075
gustavotcustodio <gustavo.torres@ufabc.edu.br>
2019-05-08 13:57:31 -0300

    test for experiments added.

--- test_experiment_framework.py
+++ test_experiment_framework.py
@@ -14,21 +14,40 @@ class TestExperimentsFramework(unittest.TestCase):
                         ]
         self.assertEqual(ef.create_grid_params(dict_params), self.expected)
 
+    def test_cluster_run_pso_experiments(self):
+        pso_params = {
+            "pop_size": [18, 36],
+            "particle_size": [18],
+            "max_iters": [50],
+            "consts": [[0.55, 1.49618, 1.49618]]
+        }
+        cluster_params = {
+            "iris":{
+                "n_clusters":[3, 6],
+                "n_attrib": 4
+            }
+        }
+        pso_results = ef.run_cluster_pso_experiments(
+                pso_params, cluster_params['iris'], 'davies_bouldin',
+                n_runs=2, dataset_name='iris')
+       # ['c1', 'c2', 'fitness', 'max_iters', 'n_clusters', 'omega', 'pop_size',
+       #'run'],
+
     def test_run_pso_experiments(self):
         # 12 possible combinations of parameters.
-        self.pso_params = {
+        pso_params = {
             "pop_size": [5, 10, 20],
             "particle_size": [10, 20],
             "max_iters": [50],
             "consts": [[0.55, 1.49618, 1.49618], [0.72, 1.49618, 1.49618]]
         }
-        self.pso_results = ef.run_pso_experiments(
-                                self.pso_params, 'rosenbrock', n_runs=5)
-        self.assertEqual(list(self.pso_results.columns), 
+        pso_results = ef.run_pso_experiments(pso_params, 'rosenbrock',
+                                             n_runs=5)
+        self.assertEqual(list(pso_results.columns), 
                         ['run','fitness','omega','c1','c2'])
         # 12 combinations of params, 50 iterations, 5 runs.
         # 12 x 50 x 5 = 3000
-        self.assertEqual(self.pso_results.shape, (3000,5))
+        self.assertEqual(pso_results.shape, (3000,5))
 
 if __name__ == '__main__':
     unittest.main()
